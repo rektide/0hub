bbad46b437e3822d7f7da210a82e20f990367c32
rektide <rektide@voodoowarez.com>
2016-10-20 01:13:06 -0400

    Migrate resource label & list webcomponents to es2015 classes.

--- c/resourceLabel.js
+++ c/resourceLabel.js
@@ -1,43 +1,72 @@
-var ResourceLabel= Object.create(HTMLElement.prototype, {
-	refresh: {
-		enumerable: true,
-		value: function(){
-			var src= this.getAttribute("src")
-			return fetch(this.getAttribute("src")).then(function(response){
-				this.data= response.text().then(function(data){
-					try{
-						data= JSON.parse(data)
-					}catch(ex){}
-					this.innerHTML = JSON.stringify(data)
-					return data
-				})
-			})
+class ResourceLabel extends HTMLElement {
+	constructor(){
+		super()
+	}
+	static get observedAttributes(){
+		return ["src"]
+	}
+	connectedCallback(){
+		var src= this.getAttribute("src")
+		if(src){
+			this.refresh()
 		}
-	},
-	attributeChangedCallback: {
-		value: function(name, oldValue, newValue){
-			if(name === "src"){
-				this.refresh()
-			}else{
-				console.error("unexpected attribute changed event: "+name)
-			}
-		}
-	},
-	connectedCallback: {
-		value: function(){
-			this.innerHTML= "[loading]"
-		}
-	},
-	createdCallback: {
-		value: function(){
-			var src= this.getAttribute("src")
-			if(src){
-				this.attributeChangedCallback("src", undefined, src)
-			}
-			this.innerHTML= "[loading]"
+		this.innerHTML = "[loading]"
+	}
+	refresh(){
+		this.data= fetch(this.getAttribute("src"))
+		  .then(response=> response.json())
+		this.data.then(data=> {
+			this.innerHTML= data
+		})
+	}
+	attributedChangedCallback(name){
+		if(name === "src"){
+			this.refresh()
+		}else{
+			console.error("unexpected attribute changed event: "+name)
 		}
 	}
-})
+}
+
+//	refresh: {
+//		enumerable: true,
+//		value: function(){
+//			var src= this.getAttribute("src")
+//			return fetch(this.getAttribute("src")).then(function(response){
+//				this.data= response.text().then(function(data){
+//					try{
+//						data= JSON.parse(data)
+//					}catch(ex){}
+//					this.innerHTML = JSON.stringify(data)
+//					return data
+//				})
+//			})
+//		}
+//	},
+//	attributeChangedCallback: {
+//		value: function(name, oldValue, newValue){
+//			if(name === "src"){
+//				this.refresh()
+//			}else{
+//				console.error("unexpected attribute changed event: "+name)
+//			}
+//		}
+//	},
+//	connectedCallback: {
+//		value: function(){
+//			this.innerHTML= "[loading]"
+//		}
+//	},
+//	createdCallback: {
+//		value: function(){
+//			var src= this.getAttribute("src")
+//			if(src){
+//				this.attributeChangedCallback("src", undefined, src)
+//			}
+//			this.innerHTML= "[loading]"
+//		}
+//	}
+//})
 
 Object.defineProperties(ResourceLabel, {
 	observedAttributes: {
@@ -49,7 +78,7 @@ Object.defineProperties(ResourceLabel, {
 })
 
 if(typeof customElements !== "undefined"){
-	customElements.define("resource-label", ResourceLabel, {extends: "span"})
+	//customElements.define("resource-label", ResourceLabel, {extends: "span"})
 }else{
 	document.registerElement("resource-label", {
 		prototype: ResourceLabel
--- c/resourceList.js
+++ c/resourceList.js
@@ -1,58 +1,44 @@
-var ResourceList= Object.create(HTMLElement.prototype, {
-	refresh: {
-		enumerable: true,
-		value: function(){
-			return fetch(this.getAttribute("src")).then(function(response){
-				this.data= response.text().then(function(data){
-					try{
-						data= JSON.parse(data)
-						var html = ["<ol>"]
-						data.forEach(function(item){
-							html.push("<li>", item.toString(), "</li>")
-						})
-						html.push("</ol>")
-						this.innerHTML= html.join("")
-					}catch(ex){
-						data= null
-						return
-					}
-				})
+class ResourceList extends HTMLElement {
+	constructor(){
+		super()
+	}
+	static get observedAttributes(){
+		return ["src"]
+	}
+	/**
+	 * @override
+	 */
+	connectedCallback(){
+		var src= this.getAttribute("src")
+		if(src){
+			this.refresh()
+		}
+		this.innerHTML = "[loading]"
+	}
+	refresh(){
+		this.data= fetch(this.getAttribute("src"))
+		  .then(response=> response.json())
+		this.data.then(function(data){
+			var html = ["<ol>"]
+			data.forEach(function(item){
+				html.push("<li>", item.toString(), "</li>")
 			})
-		}
-	},
-	attributeChangedCallback: {
-		writable: true,
-		value: function(name, oldValue, newValue){
-			if(name === "src"){
-				this.refresh()
-			}else{
-				console.error("unexpected attribute changed event: "+name)
-			}
-		}
-	},
-	createdCallback: {
-		value: function(){
-			var src= this.getAttribute("src")
-			if(src){
-				this.attributeChangedCallback("src", undefined, src)
-			}
-			this.innerHTML= "[loading]"
+			html.push("</ol>")
+			this.innerHTML= html.join("")
+		})
+	}
+	attributedChangedCallback(name){
+		if(name === "src"){
+			this.refresh()
+		}else{
+			console.error("unexpected attribute changed event: "+name)
 		}
 	}
-
-})
-
-Object.defineProperties(ResourceList, {
-	observedAttributes: {
-		enumerable: true,
-		get: function(){
-			return ["src"]
-		}
-	}
-})
+	
+}
 
 if(typeof customElements !== "undefined"){
-	customElements.define("resource-list", ResourceList, {extends: "span"})
+	customElements.define("resource-list", ResourceList);
 }else{
 	document.registerElement("resource-list", {
 		prototype: ResourceList
